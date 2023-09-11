c2fc52c670554448a9b1a5d2db68afe8dd1ba346
Talha Rasheed <talharasheed048@gmail.com>
2023-09-11 13:51:08 +0500

    first commit

---
+++ src/components/Experience.jsx
@@ -0,0 +1,25 @@
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+
+const Experience = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done So far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+      </motion.div>
+    </>
+  );
+};
+
+export default Experience;
